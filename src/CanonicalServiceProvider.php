4b3f405472bbb17a1ac97ead1562f94cb6d48234
Filip Krzyżanowski <filip.krzyzanowski@protonmail.com>
2026-03-19 21:02:36 +0100

    Updated `Blade::aliasComponent` to use `resource_path` for improved path resolution

--- src/CanonicalServiceProvider.php
+++ src/CanonicalServiceProvider.php
@@ -56,7 +56,7 @@ final class CanonicalServiceProvider extends ServiceProvider implements Deferrab
     {
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'canonical');
 
-        Blade::aliasComponent(Canonical::class, 'canonical');
+        Blade::aliasComponent(resource_path('views/components/canonical'), 'canonical');
 
         Blade::directive('canonical', fn (?string $expression = null): string => "<?php echo sprintf('<link rel=\"canonical\" href=\"%s\" />', e(canonical()->generate($expression))); ?>");
 
